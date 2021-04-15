33b02dcc38049eaacc3041c6c8d5960db1a64d6a
MesharyAlsabaan <meshary.alsabaan@gmail.com>
2021-04-15 15:17:21 +0300

    add morgan

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
    app.useGlobalPipes(new ValidationPipe());
+   app.use(morgan('combined'))
   app.enableCors()
   await app.listen(3000);
 
